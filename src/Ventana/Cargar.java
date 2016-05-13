697fd4f3dc48f782672e5f62a785273234097ef8
Javier Riveros <javiiriveros@hotmail.com>
2016-05-13 18:09:46 -0500

    Agregada ventana de Bienvenida con el logo

---
+++ src/Ventana/Cargar.java
@@ -0,0 +1,29 @@
+package Ventana;
+
+import javax.swing.JProgressBar;
+
+public class Cargar extends Thread {
+
+    private JProgressBar progress;
+
+    public Cargar(JProgressBar progress) {
+        super();
+        this.progress = progress;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 100; i++) {
+            this.progress.setValue(i);
+            pause((int)(Math.random() * 130));
+        }
+    }
+
+    private void pause(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/Ventana/Principal.java
+++ src/Ventana/Principal.java
@@ -8,23 +8,7 @@ import java.awt.BorderLayout;
 
 public class Principal {
 
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Principal window = new Principal();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	public JFrame frame;
 
 	/**
 	 * Create the application.
@@ -38,8 +22,9 @@ public class Principal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setSize(450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
 	}
 
 }
