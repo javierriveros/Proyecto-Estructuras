5c84eec77523baaef65431d408037b693d54be85
Andrés Linares <andres.linares.971122@gmail.com>
2016-05-13 15:46:39 -0500

    Código clase Nodo
    Añadí el código y ahora el setAmplitud() tendra una condicional donde
    pedira un valor entre -1 y 1

--- src/listas/Nodo.java
+++ src/listas/Nodo.java
@@ -2,4 +2,37 @@ package listas;
 
 public class Nodo {
 
+	private float amplitud;
+	private Nodo enlace;
+	
+	public Nodo(float amplitud){
+		this.amplitud = amplitud;
+		enlace = null;
+	}
+	
+	public Nodo(float amplitud, Nodo enlace){
+		this.amplitud = amplitud;
+		this.enlace = enlace;
+	}
+	
+	public void setAmplitud(float amplitud){
+		if(amplitud >= -1 && amplitud <= 1){
+			this.amplitud = amplitud;
+		} else{
+			System.out.println("La amplitud debe estar entre -1 y 1");
+		}
+	}
+	
+	public void setEnlace(Nodo enlace){
+		this.enlace = enlace;
+	}
+	
+	public float getAmplitud(){
+		return amplitud;
+	}
+	
+	public Nodo getEnlace(){
+		return enlace;
+	}
+	
 }
