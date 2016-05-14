6755d049c9f706d658b6808dd2439aebb0649ec6
Eleazar Tejeiro <eleazartejeiro@gmail.com>
2016-05-14 18:41:01 -0500

    Ahí cambié el color :3

--- src/Ventana/Principal.java
+++ src/Ventana/Principal.java
@@ -6,15 +6,20 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
 import java.awt.Toolkit;
 import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Principal {
 
@@ -34,13 +39,13 @@ public class Principal {
 		frmAuditorAlpha = new JFrame();
 		frmAuditorAlpha.getContentPane().setBackground(Color.WHITE);
 		frmAuditorAlpha.setTitle("AudiTor");
-		frmAuditorAlpha.setBounds(100, 100, 450, 300);
+		frmAuditorAlpha.setBounds(100, 100, 708, 417);
 		frmAuditorAlpha.setLocationRelativeTo(null);
-		
+
 		frmAuditorAlpha.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Image icon = Toolkit.getDefaultToolkit().getImage("/src/img/icon-AudiTor.png");
 		frmAuditorAlpha.setIconImage(icon);
-		
+
 		elMenuBar();
 		decoracionVentana();
 	}
@@ -78,18 +83,41 @@ public class Principal {
 		frmAuditorAlpha.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(new GridLayout(2, 1, 0, 0));
 
-		JButton btnNewButton = new JButton();
-		btnNewButton.setBackground(Color.WHITE);
-		panel.add(btnNewButton);
+		JButton btnAgregar = new JButton();
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evento) {
+				try {
+					JFileChooser cargaArchivo = new JFileChooser();
+					cargaArchivo.showOpenDialog(null);
+					String ruta = cargaArchivo.getSelectedFile().getAbsolutePath();
+					if (ruta.contains(".mp3") || ruta.contains(".wav") || ruta.contains(".aiff")) {
+
+						JOptionPane.showMessageDialog(null, "Archivo cargado exitosamente.");
+
+						frmAuditorAlpha.dispose();
+					} else {
+						JOptionPane.showMessageDialog(null, "Extensión de archivo no compatible", "Error",
+								JOptionPane.ERROR_MESSAGE);
+					}
+				} catch (Exception ex) {
+					System.out.println(ex);
+					JOptionPane.showMessageDialog(null, "No has seleccionado ningún archivo.", "Aviso",
+							JOptionPane.WARNING_MESSAGE);
+				}
+
+			}
+		});
+		btnAgregar.setBackground(Color.WHITE);
+		panel.add(btnAgregar);
 
 		// Image iconButton =
 		// Toolkit.getDefaultToolkit().getImage("src/icon.png");
-		btnNewButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/iconx96.png"))); // NOI18N
-		btnNewButton.setBorder(null);
-		btnNewButton.setBorderPainted(false);
-		btnNewButton.setContentAreaFilled(false);
-		btnNewButton.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icon-pressedx96.png"))); // NOI18N
-		btnNewButton.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icon-hoverx96.png"))); //
+		btnAgregar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/iconx96.png"))); // NOI18N
+		btnAgregar.setBorder(null);
+		btnAgregar.setBorderPainted(false);
+		btnAgregar.setContentAreaFilled(false);
+		btnAgregar.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icon-pressedx96.png"))); // NOI18N
+		btnAgregar.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icon-hoverx96.png"))); //
 
 		JLabel lblCargarArchivo = new JLabel("Cargar Archivo");
 		lblCargarArchivo.setFont(new Font("RobotoDraft", Font.ITALIC, 26));
